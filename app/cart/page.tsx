1a8e1ffa7a8038756e4c2183016156ce5096b513
Amine <75739071+Poet9@users.noreply.github.com>
2023-11-11 21:59:15 +0100

    fixed object fit issue

---
+++ app/cart/page.tsx
@@ -0,0 +1,8 @@
+// b67a3f17-2b7d-450f-b0b8-c98775e3413e
+import React from "react";
+
+function Cart() {
+    return <div className="pt-36">you are in cart page dude</div>;
+}
+
+export default Cart;
---
+++ components/cartCard/index.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import React from "react";
+
+function CartCard({
+    data,
+    popupFunc,
+    ...props
+}: {
+    data: {
+        id: string;
+        title: string;
+        image: string;
+        description: string | undefined;
+        price: string | number;
+    };
+    popupFunc: (data: any) => void;
+}) {
+    return (
+        <div
+            onClick={popupFunc}
+            id={data.id}
+            className="flex flex-col w-64 shadow-2xl shadow-indigo-700/50 bg-zinc-200/25 cursor-pointer rounded-md border-2 border-zinc-200/25"
+        >
+            <Image
+                src={data.image}
+                alt={"product image"}
+                width={256}
+                height={256}
+                className="object-cover rounded-t-lg w-64 h-48"
+            />
+            <h2 className="h-8 p-2 w-52  truncate text-lg">{data.title.toUpperCase()}</h2>
+            <p className=" h-16 p-2 text-sm opacity-50 overflow-y-hidden">
+                {data.description || ""}
+            </p>
+            <span className=" w-fit  ml-auto mx-4 p-1 rounded-xl border-1 bg-stone-500/25">
+                {data.price || 502} <strong>DZD</strong>
+            </span>
+        </div>
+    );
+}
+
+export default CartCard;
