62af53106644379290b6d96880e2c018970808e4
Amine <75739071+Poet9@users.noreply.github.com>
2023-11-16 12:56:00 +0100

    added delete method to the dashboard

--- components/table/helper.ts
+++ components/table/helper.ts
@@ -1,7 +1,7 @@
 // function to delete a row
 export const deleteRow = async (id: string) => {
     try {
-        const res = await fetch(`http://localhost:3000/api/category/`, {
+        const res = await fetch(`http://localhost:3000/api/category`, {
             method: "DELETE",
             body: JSON.stringify({ categoryId: id }),
         });
